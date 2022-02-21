0b4b307e391d9ee99997c9d2cd35f879006e451b
Eddy Varela <eddy@hellosage.com>
2022-02-21 12:39:38 -0500

    second commit

---
+++ Able-iOS/Able_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Able_iOSApp.swift
+//  Able-iOS
+//
+//  Created by Eddy Varela on 2/21/22.
+//
+
+import SwiftUI
+
+@main
+struct Able_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
